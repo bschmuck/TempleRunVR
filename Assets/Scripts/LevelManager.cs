529baa844bc582300171be09e02db66bf539e280
Brandon Schmuck <bschmuck@BrandonMacBook4.wv.cc.cmu.edu>
2017-01-15 14:37:23 -0500

    Initial Commit.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			NextScene ();
+		}
+	}
+
+	void NextScene() {
+		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex + 1);
+	}
+
+	void PrevScene() {
+		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex - 1);
+	}
+}
